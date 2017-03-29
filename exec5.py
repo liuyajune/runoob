5f8ce695970b242253af9757d872d0a510dd9275
liuyajune <liuyajune@126.com>
2017-03-29 16:29:45 +0800

    exec5

--- exec5.py
+++ exec5.py
@@ -4,9 +4,16 @@
 题目：列表转换为字典。
 
 程序分析：无。
+zip()  lambda   map()  filter  sorted
 '''
 
-list1 = [1,2,3,4]
+# list1 = [1,2,3,4,5]
+# list2 = ['a','b','c','d']
+# dict1 = dict(zip(list1,list2))
+# print(dict1)
+
+list1 = [1,2,3,4,5]
 list2 = ['a','b','c','d']
-dict1 = dict([list1,list2])
-print(dict1)
+dict2 = dict(map(lambda x,y:[x,y], list1,list2))
+
+print(dict2)
