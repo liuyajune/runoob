c9705d578adf177a19c81ceaf059367640db260f
liuyajune <liuyajune@126.com>
2017-03-30 08:47:54 +0800

    计时器

--- exec9.py
+++ exec9.py
@@ -4,4 +4,14 @@
 题目：暂停一秒输出。
 
 程序分析：无。
+注意：dict1的值输出并不按顺序
 '''
+import time
+dict1 = {'a':'hello', 'b':'world', 'c':'test'}
+for  key, value in dict1.items():
+    print('key:', key , 'value:', value)
+    time.sleep(2)
+
+
+for key in dict1.keys():
+    print(key)
