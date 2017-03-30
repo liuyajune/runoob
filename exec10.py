f98a059c423220b371e00b5da811565851361bfc
liuyajune <liuyajune@126.com>
2017-03-30 08:55:01 +0800

    计时格式

--- exec10.py
+++ exec10.py
@@ -5,3 +5,12 @@
 
 程序分析：无。
 '''
+
+import time
+
+print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+# 暂停一秒
+time.sleep(1)
+
+print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
