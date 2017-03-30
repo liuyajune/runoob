f6d65b0c77c0ca89f8c9c179c117bfe751ff6526
liuyajune <liuyajune@126.com>
2017-03-30 08:27:40 +0800

    使用类方法实现

--- exec8.py
+++ exec8.py
@@ -7,8 +7,27 @@
 psr-4  php代码编写规范
 pep8  python代码编写规范
 '''
-for i  in range(1, 10):
-    print()
-    for j in range(1, 10):
-        if i <= j:
-            print("%d * %d = %d "% (i, j, i * j), end=" ")
+# for i  in range(1, 10):
+#     print()
+#     for j in range(1, 10):
+#         if i <= j:
+#             print("%d * %d = %d "% (i, j, i * j), end=" ")
+
+
+class  MultiFunc:
+    def __init__(self,m=9, n=9):
+        self.line = m
+        self.row = n
+
+    def multi(self):
+        m,n = self.line, self.row
+        for i  in range(1, m + 1):
+            print()
+            for j in range(1, n + 1):
+                if i <= j:
+                    print("%d * %d = %d "% (i, j, i * j), end=" ")
+
+
+
+myMulti = MultiFunc(9, 9)
+myMulti.multi()
